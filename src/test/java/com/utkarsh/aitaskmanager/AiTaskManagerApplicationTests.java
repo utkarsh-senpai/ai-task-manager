a7d2562f4375673bfe7b6eb50c603f9b791d414f
utkarsh-zendesk <utkarsh.meshram@zendesk.com>
2025-08-10 05:33:59 +0530

    feat: add initial Spring Boot backend boilerplate

---
+++ src/test/java/com/utkarsh/aitaskmanager/AiTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utkarsh.aitaskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
